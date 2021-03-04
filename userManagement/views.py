fa09f232e9156429ec414520e2172d2433884b32
Asif Ahmed <asif.ahmed@datasoft-bd.com>
2021-03-04 17:48:59 +0600

    First Commit

---
+++ userManagement/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+
+
+def login(request):
+    if request.user.username == 'asif':
+        return HttpResponse("<h1>Auth</h1>")
+    return HttpResponse("<h1>Unauth</h1>")
+
+
+def dashboart(request):
+    if request.user.username == 'asif':
+        return HttpResponse("<h1>Auth</h1>")
+    return HttpResponse("<h1>Unauth</h1>")
